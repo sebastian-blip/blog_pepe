6b27f4b07531520c696735f353a6f1f4353693de
sebastian.betancourth <sebastian.betancourth@quipux.com>
2022-09-20 23:23:29 -0500

    feat:se crean el ingreso

--- Pepe/Pepeescalada/urls.py
+++ Pepe/Pepeescalada/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import feed, profile, registro
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.views import LoginView
+from .views import feed, profile, registro
 
 
 urlpatterns = [
     path('', feed, name='feed'),
     path('profile/', profile, name='profile'),
     path('registro/', registro, name='registro'),
+    path('login/', LoginView.as_view(template_name='blog/ingreso.html'),
+         name='ingreso')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
